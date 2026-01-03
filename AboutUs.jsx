a22e9928f6ed9097e58c8bae2149a505948bfc1e
David <109250396+David528785502@users.noreply.github.com>
2026-01-03 13:05:49 -0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant store dedicated to bringing beautiful
+        and healthy indoor plants to homes everywhere. We believe plants improve
+        quality of life and create peaceful environments.
+      </p>
+      <p>
+        Our mission is to provide high-quality plants, helpful care information,
+        and an easy-to-use shopping experience for plant lovers of all levels.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
